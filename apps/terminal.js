8893428d5887e298573879fe2b974ef2d6e9043d
shitten <kiefciman@gmail.com>
2025-12-16 22:04:57 +0200

    copypasta + help terminal commands

--- apps/terminal.js
+++ apps/terminal.js
@@ -2,7 +2,6 @@ const content = document.querySelector('#content')
 const form = document.querySelector('#form')
 const input = document.querySelector('#input')
 
-console.log('a')
 input.focus()
 input.select()
 
@@ -14,9 +13,57 @@ form.addEventListener(
 )
 
 function ProcessInput() {
+    if(input.value.length == 0) return
+
     content.innerHTML += '<div class = "output">$ ' + input.value + '</div>'
-    console.log('a')
+
+    switch(input.value) {
+        case 'help':
+            Help()
+            break
+        case 'copypasta':
+            Copypasta()
+            break
+        default:
+            let c = '<div class = "output">Command not found. '
+            c += 'Type "help" to show available commands</div>'
+            content.innerHTML += c
+            break
+    }
 
     form.reset()
     form.scrollIntoView({behavior: 'instant'})
+}
+
+function Copypasta() {
+    let i = 0
+    let c = '<div class = "output">'
+
+    fetch('https://reddit-rss-api.deno.dev/r/copypasta')
+        .then(r => r.json())
+        .then(r => r.items)
+        .then(r => {
+            i = Math.floor(Math.random() * (r.length - 1))
+            c += r[i].title + '<br>'
+            c += 'by ' + r[i].author + '<br><br>'
+            c += r[i].message + '</div>'
+            content.innerHTML += c
+        })
+}
+
+function Help() {
+    const help = [
+        'Available commands:',
+        'copypasta - random post from r/copypasta',
+    ]
+
+    let c = '<div class = "output">'
+
+    for(let i = 0; i < help.length; i++) {
+        if(i > 0) c += '- '
+        c += help[i] + '<br>'
+    }
+
+    c += '</div>'
+    content.innerHTML += c
 }
